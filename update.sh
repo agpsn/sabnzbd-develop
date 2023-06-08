5bc8eff23d4987a539722e6708f71a68aab40841
AGPSN <github@agpsn.com>
2023-06-08 07:40:27 -0500

    Updated

--- update.sh
+++ update.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -eu
 
-[ ! -d "/mnt/user/system/agpsn-github/sabnzbd-develop" ] && echo "No repo!" && exit 1
-cd "/mnt/user/system/agpsn-github/sabnzbd-develop"
-
-echo $(cat ~/.ghcr-token) | docker login ghcr.io -u $(cat ~/.ghcr-user) --password-stdin &>/dev/null
-
 NVERSION=$(curl -sL "https://api.github.com/repos/sabnzbd/sabnzbd/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]')
 
-echo "Building and Pushing 'ghcr.io/agpsn/docker-sabnzbd:$NVERSION'"
-docker build --quiet  --force-rm --rm --tag ghcr.io/agpsn/docker-sabnzbd:develop --tag ghcr.io/agpsn/docker-sabnzbd:$NVERSION -f ./Dockerfile.develop .
-docker push --quiet ghcr.io/agpsn/docker-sabnzbd:develop; docker push --quiet ghcr.io/agpsn/docker-sabnzbd:$NVERSION && docker image rm -f ghcr.io/agpsn/docker-sabnzbd:$NVERSION
+echo $(cat ../.token) | docker login ghcr.io -u $(cat ../.user) --password-stdin &>/dev/null
+
+echo "Updating Sabnzbd: v$NVERSION"
+docker build --quiet --force-rm --rm --tag ghcr.io/agpsn/docker-sabnzbd:develop --tag ghcr.io/agpsn/docker-sabnzbd:$NVERSION -f ./Dockerfile.develop .
 git tag -f $NVERSION && git push --quiet origin $NVERSION -f --tags
 git add . && git commit -m "Updated" && git push --quiet
+docker push --quiet ghcr.io/agpsn/docker-sabnzbd:develop; docker push --quiet ghcr.io/agpsn/docker-sabnzbd:$NVERSION && docker image rm -f ghcr.io/agpsn/docker-sabnzbd:$NVERSION
 echo ""
